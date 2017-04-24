08f65c762dba4ffe78ef09920d3b6e6aac967fbd
mariam <mariamdarwish142@gmail.com>
2017-04-25 00:46:01 +0200

    delete

--- app/Model/index.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/sprint2");
-
-mongoose.set("debug", true);
-
-module.exports.User = require("./user");
-module.exports.Customer = require("./customer");
